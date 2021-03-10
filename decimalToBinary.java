015a6fac2c3895ff5bea4d4fc5909ebcaab75e7d
Francis <poste18@MJL.local>
2021-03-10 13:24:21 -0500

    création du programme conversion decimal binary

---
+++ decimalToBinary.java
@@ -0,0 +1,27 @@
+public class decimalToBinary {
+    
+
+    static String decimalToBinaryFN( String number){
+        String resultat = "";
+
+        for(int i = 0; i<100; i++){
+            String entier = "0";
+            double reste = 0.0;
+
+            
+
+
+            if (reste == 0.0){
+                break;
+            }
+            
+            resultat += entier;
+        }
+
+        return resultat;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
