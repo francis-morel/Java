b9df4df432ee982276c0a16ba3a08533a0965c25
Francis <poste18@MJL.local>
2021-03-10 14:33:56 -0500

    Finition de la function et utilisable avec argument

--- decimalToBinary.java
+++ decimalToBinary.java
@@ -1,21 +1,28 @@
 public class decimalToBinary {
     
 
-    static String decimalToBinaryFN( String number){
-        String resultat = "";
+    static String decimalToBinaryFN( double number){
+        String resultat = "0.";
+        double reste = number;
 
-        for(int i = 0; i<100; i++){
+        for(int i = 0; i<1000; i++){
             String entier = "0";
-            double reste = 0.0;
-
             
 
+            reste *= 2.0;
+            if(reste >= 1.0){
+                entier = "1";
+                reste -= 1.0;
+            }
+            else{
+                entier = "0";
+            }
+
+            resultat += entier;
 
             if (reste == 0.0){
-                break;
+               break;
             }
-            
-            resultat += entier;
         }
 
         return resultat;
@@ -23,5 +30,6 @@ public class decimalToBinary {
 
     public static void main(String[] args) {
 
+        System.out.println(decimalToBinaryFN(Double.parseDouble(args[0])));
     }
 }
