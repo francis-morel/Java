319aadb99e9b77e4aa5d604d511f7883e6f62e3a
Francis <poste18@MJL.local>
2021-03-11 12:19:22 -0500

    Optimisation du code selon mathzx

--- BinaryToHexa.java
+++ BinaryToHexa.java
@@ -1,10 +1,10 @@
 public class BinaryToHexa {
 
-    private static String hexaValue(String search){
+    private static String hexaValue(String search) {
         String[] hexaValues = {"0000", "0001", "0010", "0011", "0100", "0101", "0110", "0111", "1000", "1001", "1010", "1011", "1100", "1101", "1110", "1111"};
         String[] hexaIndex = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"};
 
-        for(int i = 0; i < hexaValues.length; i++){
+        for(int i = 0; i < hexaValues.length; i++) {
             if(search.equals(hexaValues[i])){           
                 return hexaIndex[i];
             }
@@ -12,26 +12,30 @@ public class BinaryToHexa {
         return "-1";
     }
 
-    private static String correctLeadingBinary(String binary){
-        if(binary.length() % 4 == 1){
-            binary = "000" + binary;
-        }
-        else if(binary.length() % 4 == 2){
-            binary = "00" + binary;
-        }
-        else if(binary.length() % 4 == 1){
-            binary = "0" + binary;
+    private static String correctLeadingBinary(String binary) {
+        switch(binary.length() % 4){
+            case 0:
+            return binary;
+            
+            case 1:
+            return "000" + binary;
+
+            case 2:
+            return "00" + binary;
+
+            case 3:
+            return "0" + binary;
         }
         return binary;
     }
 
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         String binary = args[0];
         String binaryCorrected = correctLeadingBinary(binary);
         String hexaReturn = "";
 
-        for(int i = 0; i <= binaryCorrected.length() -4; i += 4){
+        for(int i = 0; i <= binaryCorrected.length() -4; i += 4) {
             hexaReturn += hexaValue(binaryCorrected.substring(i, i+4));
         }
         System.out.println(hexaReturn);
