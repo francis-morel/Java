1365ba19918a869292dd14acd0269cc4f6cb3429
Francis <poste18@MJL.local>
2021-03-18 13:20:35 -0400

    travail sur tp1

--- cours java/tp1.java
+++ cours java/tp1.java
@@ -1,17 +1,18 @@
 import java.lang.Math;
+import java.util.Scanner;
 
 public class tp1{
 
     public static double piCalcul(int nombreIteration) {
-        double value = 0.0f;
+        double value = 0.0;
 
         for(int i = 1; i <= nombreIteration; i++) {
             double denominateur = (double)i;
-            value += (1 / (denominateur * denominateur));
+            value += (1.0 / (denominateur * denominateur));
 
         }
 
-        return Math.sqrt(value*6);
+        return Math.sqrt(value*6.0);
     }
 
     public static boolean isValidInt(String valeur) {
@@ -26,14 +27,58 @@ public class tp1{
         return true;
     }
 
+    public static float rendementPi(double value) {
+        float rendement = 0.0f;
+
+        String valueStr = String.valueOf(value);
+        char[] valueArray = valueStr.toCharArray();
+        String piStr = String.valueOf(Math.PI);
+        char[] piArray = piStr.toCharArray();
+
+        int piLength = piArray.length;
+
+        for(int i = 0; i<10; i++) {
+            //si le premier chiffre n'est pas 3, retourner 0%
+            //Le premier chiffre après la virgule vaut 80% de rendement
+            //Les chiffres suivants ont de moins en moins de valeur
+            /*
+            if(valueArray[0] != '3'){
+                return 0.0f;
+            }
+
+            if(valueArray.length >= i) {
+                if (piArray[i] == valueArray[i]) {
+                    rendement += 1.0f;
+                }
+            }
+            */
+            System.out.print(valueArray[i]);
+        }
+        System.out.print('\n');
+        
+        rendement = rendement/10.0f*100.0f;
+
+        return rendement;
+        
+    }
+
     public static void main(String[] args) {
-        isValidInt(args[0]);
-        /*int nombreIteration = Integer.parseInt(args[0]);
-        System.out.println(nombreIteration);
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Entrez le nombre d'itérations désirées: ");
+        int nombreIteration = Integer.parseInt(scan.nextLine());
         double piValue = piCalcul(nombreIteration);
 
         System.out.println("Calcul de pi avec " + nombreIteration + " itterations:");
-        System.out.println(piValue);*/
+        System.out.println(piValue);
+
+        scan.close();
+
+        float rendement = rendementPi(piValue);
+
+        System.out.println(rendement);
+               
+
 
     }
 }
