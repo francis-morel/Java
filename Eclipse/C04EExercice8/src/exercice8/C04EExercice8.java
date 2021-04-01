58383b1c697142e12a019fb65babb0ff78d27e7c
Your Name <you@example.com>
2021-04-01 19:09:39 -0400

    test commit

---
+++ Eclipse/C04EExercice8/src/exercice8/C04EExercice8.java
@@ -0,0 +1,38 @@
+package exercice8;
+
+import java.util.Scanner;
+
+public class C04EExercice8 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Moyenne: ");
+		double moyenne = askDouble(scanner);
+		
+		System.out.print("Écart-type: ");
+		double ecartType = askDouble(scanner);
+		
+		System.out.print("Valeur: ");
+		double valeur = askDouble(scanner);
+		
+		double result = Math.pow(1.0/(ecartType * Math.sqrt(2.0 * Math.PI)) , -1.0/2.0 * Math.pow((valeur - moyenne) / ecartType, 2.0));
+		
+		double result2  = (1.0 / (ecartType * Math.sqrt(2 * Math.PI))) * Math.exp(-(1.0/2.0) * Math.pow((valeur - moyenne)/ecartType, 2));
+		
+		System.out.println(result);
+	}
+	
+	public static double askDouble(Scanner scanner) {
+		double retour = 0.0;
+		do {
+			System.out.print("? ");
+			if(scanner.hasNextDouble()) {
+				retour = scanner.nextDouble();
+			}
+			scanner.nextLine();
+		} while (retour < 0.0001);
+		
+		return retour;
+	}
+
+}
