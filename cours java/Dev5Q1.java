28c01732adb25200d1afd465e4c1f124a203aad9
Francis <poste18@MJL.local>
2021-04-12 14:38:13 -0400

    devoir #5

---
+++ cours java/Dev5Q1.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Dev5Q1 {
+    public static void main(String[] args) {
+        Scanner clavier = new Scanner(System.in);
+        System.out.print("Entrez une valeur paire entre 1 et 99 : ");
+        int entree = -1;
+
+        if (clavier.hasNextInt()) {
+            entree = clavier.nextInt();
+        }
+
+        clavier.close();
+        boolean retour = determinerSiValide(entree);
+
+        if (retour == true) {
+            System.out.println("La valeur entrée est valide.");
+        } else {
+            System.out.println("La valeur entrée n'est pas valide.");
+        }
+    }
+    public static boolean determinerSiValide(int valeur) {
+        if (valeur>0) {
+            if (valeur<100) {
+                if (valeur%2==0) {
+                    return true;
+                }
+            }
+        }
+        return false; //Q1a. le code ne compile pas parce que la fonction ne retournait rien si la valeur n'est pas valide et voici la correction.
+    }
+}
---
+++ cours java/Dev5Q2.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class Dev5Q2 {
+    public static void main(String[] args) {
+        Scanner clavier = new Scanner(System.in);
+
+        int v1 = obtenirEntier(clavier, "V1");
+        int v2 = obtenirEntier(clavier, "V2");
+        int v3 = obtenirEntier(clavier, "V3");
+        clavier.close();
+
+        int valeurMax = valeurMaximale(v1, v2, v3);
+        double moyenne = calculerMoyenne(v1, v2, v3);
+
+        afficherResultats(valeurMax, moyenne);
+    }
+
+    public static int obtenirEntier(Scanner lecteur, String nom) {
+        int valeur = -1;
+
+        do {
+            System.out.print("Entrez un entier " + nom + "entre 1 et 20 : ");
+            if (lecteur.hasNextInt()) {
+                valeur = lecteur.nextInt();
+            }
+            lecteur.nextLine();
+        } while (valeur < 1 || valeur > 20);
+
+        return valeur;
+    }
+
+    public static int valeurMaximale(int v1, int v2, int v3) {
+        int max = -1;
+        
+        if (v1 > max) max = v1;
+        if (v2 > max) max = v2;
+        if (v3 > max) max = v3;
+
+        return max;
+    }
+
+    public static double calculerMoyenne(int v1, int v2, int v3) {
+        double moyenne = (v1 + v2 + v3) / 3.0;
+
+        return moyenne;
+    }
+
+    public static void afficherResultats(int max, double moyenne) {
+        System.out.println("La valeur max: " + max);
+        System.out.println("La moyenne: " + moyenne);
+    }
+
+}
