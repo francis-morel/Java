69c77b8f8c7904122ead719149cc923ee2ecdc6c
Francis <poste18@MJL.local>
2021-04-20 14:00:36 -0400

    ajout des fichiers du devoir 6

---
+++ cours java/Dev6Q1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Dev6Q1 {
+    public static void main(String[] args) {
+        Scanner clavier = new Scanner(System.in);
+        int premier = obtenirEntier(clavier, "comme première valeur");
+        int deuxieme = obtenirEntier(clavier, "comme deuxième valeur");
+        clavier.close();
+        int somme = premier + deuxieme;
+        System.out.println("Somme = " + somme);
+    }
+
+    public static int obtenirEntier(Scanner lecteur, String nom) {
+        int valeur = -1000;
+        do {
+            System.out.print("Entrez un entier " + nom + " entre -10 et +10 : ");
+            if (lecteur.hasNextInt()) {
+                valeur = lecteur.nextInt();
+            }
+            // On vide le tampon
+            lecteur.nextLine();
+        } while (valeur < -10 || valeur > 10);
+        return valeur;
+    }
+}
